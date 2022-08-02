b2df10a31aeaf5cf7dc89a1c56493890f8986a1f
intinek <102937646+intinek@users.noreply.github.com>
2022-08-02 00:24:28 -0300

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,10 @@
+public class ejercicio1 {
+    public static  void main(String[] args){
+        int resultado;
+        resultado = suma(a 10, b 30);
+        System.out.println(resultado);
+    }
+    public static int suma(int a, int b){
+        return a + b;
+    }
+}
