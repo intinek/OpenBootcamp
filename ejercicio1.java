1418a8aac51de110ef13e6dd7b03d117dc073e02
intinek <102937646+intinek@users.noreply.github.com>
2022-08-02 00:56:11 -0300

    Update ejercicio1.java

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,10 +1,13 @@
-public class ejercicio1 {
-    public static  void main(String[] args){
-        int resultado;
-        resultado = suma(a 10, b 30, c 40);
-        System.out.println(resultado);
+public class ejercicio1{
+
+    public static void main(String[] args){
+        int resultado = suma(1,2,3);
+        System.out.println("resultado: "+resultado);
     }
-    public static int suma(int a, int b, int c){
-        return a + b + c;
+
+        
+    public static int suma(int num1, int num2, int num3 ){
+        return num1+num2+num3;
     }
+
 }
